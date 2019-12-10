79d884dba5d9fa1f5e04de9acdae24d05a1600c0
mrAppleXZ <mr.applexz@gmail.com>
2019-12-10 20:56:50 +0500

    Initial commit

---
+++ src/main/kotlin/net/pearx/craftdumper/Reference.kt
@@ -0,0 +1,12 @@
+package net.pearx.craftdumper
+
+const val ID = "craftdumper"
+const val NAME = "CraftDumper"
+const val DESCRIPTION = ""
+const val VERSION = ""
+const val ACCEPTED_MINECRAFT_VERSIONS = ""
+const val DEPENDENCIES = ""
+val AUTHORS = listOf("mrAppleXZ")
+
+
+const val PROJECTE_ID = "projecte"
